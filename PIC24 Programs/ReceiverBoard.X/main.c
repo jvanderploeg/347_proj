a3992cee26eb2460b67dc09c3c97e3f1d57b9e21
James Paul <jayarepea23@aol.com>
2013-05-30 14:38:44 -0500

    Created Receiver Board Code

---
+++ PIC24 Programs/ReceiverBoard.X/main.c
@@ -0,0 +1,23 @@
+/* 
+ * File:   main.c
+ * Author: James Paul
+ *
+ * Created on May 30, 2013, 2:10 PM
+ */
+
+#include <p24Exxxx.h>
+
+// PIC24EP256GP204 Configuration Bit Settings
+// TODO: Create these from Window > PICMemoryViews > ConfigurationBits
+//
+
+#include <xc.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return(0);
+}
+
